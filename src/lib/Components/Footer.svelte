b70c60cdd0036a6eea962a8b735775f8d11fe61a
justbarnt <functnal@gmail.com>
2024-08-03 14:08:06 -0400

    feat: footer is complete and responsive

--- src/lib/Components/Footer.svelte
+++ src/lib/Components/Footer.svelte
@@ -4,13 +4,15 @@
 
 <main class="flex flex-col">
   <div class="flex justify-center items-center font-neonoir text-2xl bg-primary-500 align-middle mx-auto w-full p-2">Dogs By Nature ©️{new Date().getFullYear()}</div>
-  <div class="grid lg:grid-cols-[auto_auto_auto] md:grid-rows bg-tertiary-500 justify-around">
+  <div class="footer-lg-styles flex flex-col justify-around bg-tertiary-500">
     {#each Footer_Items as item}
-      <div class="flex flex-col gap-2">
-        <h3 class="h3 font-neonoir tracking-wide text-white underline underline-offset-[10px] decoration-4 decoration-secondary-500">{item.title}</h3>
-        {#each item.items as i}
-          <p class="font-ostrich_black tracking-[0.1em] leading-snug text-white text-base">{i}</p>
-        {/each}
+      <div class="flex flex-col gap-2 lg:bg-tertiary-500 lg:even:bg-tertiary-500 even:bg-tertiary-700 p-4">
+        <h3 class="text-2xl font-neonoir tracking-wide text-white border-b-4 border-secondary-500 pb-2">{item.title}</h3>
+        <div class="grid grid-cols-3 lg:flex lg:flex-col">
+          {#each item.items as i}
+            <p class="font-ostrich_black tracking-[0.1em] leading-snug text-white text-base whitespace-pre-line">{i}</p>
+          {/each}
+        </div>
       </div>
     {/each}
   </div>
