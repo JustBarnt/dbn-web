957d9b6d0e7e778301952609575b757c75514be3
justbarnt <functnal@gmail.com>
2024-08-30 08:07:27 -0400

    continued work on classes page

--- src/routes/classes/+page.svelte
+++ src/routes/classes/+page.svelte
@@ -1,7 +1,91 @@
 <script>
   import Banner from "$lib/Components/Banner.svelte";
+  import site_content from "$lib/Components/Content/content.json";
+  import IconBanner from "$lib/assets/our-classes/OurClasses_Icons.png";
+  import ClassesCard from "./ClassesCard.svelte";
+  import BronzeImg from "$lib/assets/our-classes/OurClasses_Private_Bronze.png";
+  import SilverImg from "$lib/assets/our-classes/OurClasses_Private_Silver.png";
+  import GoldImg from "$lib/assets/our-classes/OurClasses_Private_Gold.png";
+  import { onMount } from "svelte";
+
+  const page_content = site_content["Our Classes"];
+  const packages = page_content.Packages;
+
+  /** @param pkgs {typeof page_content.Packages} */
+  const CardGenerator = (pkgs) => {
+    const ret = pkgs.map((pkg) => {
+      return {
+        Title: pkg.Title,
+        Image: pkg.Image,
+        Price: pkg.Price,
+        Details: pkg.Details,
+        Additional: pkg.Additional
+      };
+    });
+
+    return ret;
+  };
 </script>
 
-<content>
+<main>
   <Banner />
-</content>
+  <section
+    id="Private-Lessons"
+    class="flex flex-col lg:px-24 lg:py-16 bg-white gap-y-12">
+    <div class="w-3/4 self-center">
+      <img
+        src={IconBanner}
+        alt="Banner of Icons Illustrating Classes Offered" />
+    </div>
+    <div class="flex flex-col gap-8">
+      <div>
+        <h1 class="font-ostrich_med text-5xl text-center">{Object.keys(page_content)[0]}</h1>
+        <p class="text-sm w-[95%]">{page_content["Private Lessons"]}</p>
+      </div>
+      <div class="flex lg:flex-row flex-col lg:gap-x-8">
+        {#each packages as item}
+          <ClassesCard
+            card_points={item.Details}
+            card_title={item.Title}
+            price={item.Price}
+            additional_info={item.Additional}>
+            <div slot="image">
+              <img
+                class="mx-auto"
+                width="75%"
+                src={item.Image}
+                alt="" />
+            </div>
+          </ClassesCard>
+        {/each}
+      </div>
+    </div>
+  </section>
+  <section
+    id="Dog-Walks"
+    class="flex flex-col lg:px-24 lg:py-16 bg-surface-50">
+    <div class="flex flex-col gap-8">
+      <div class="flex flex-col items-center">
+        <h1 class="font-ostrich_med text-5xl text-center">{Object.keys(page_content)[2]}</h1>
+        <p class="text-sm w-[95%]">{page_content["Dog Walks"]}</p>
+      </div>
+      <div class="flex lg:flex-row flex-col lg:gap-x-8">
+        {#each packages as item}
+          <ClassesCard
+            card_points={item.Details}
+            card_title={item.Title}
+            price={item.Price}
+            additional_info={item.Additional}>
+            <div slot="image">
+              <img
+                class="mx-auto"
+                width="75%"
+                src={item.Image}
+                alt="" />
+            </div>
+          </ClassesCard>
+        {/each}
+      </div>
+    </div>
+  </section>
+</main>
---
+++ src/routes/classes/ClassesCard.svelte
@@ -0,0 +1,31 @@
+<script>
+  import PawBP from "$lib/assets/our-classes/OurClasses_PawprintBulletPoint.png";
+
+  /** @type string */
+  export let card_title;
+  /** @type string */
+  export let price;
+  /** @type string[] */
+  export let card_points;
+  /** @type string */
+  export let additional_info = "";
+</script>
+
+<div class="card shadow-lg w-1/3">
+  <div class="flex flex-col p-4 items-center gap-y-2">
+    <slot name="image" />
+    <h2 class="font-neonoir h2">{card_title}</h2>
+    <h3 class="font-ostrich_black h3 text-primary-500">{price}</h3>
+    <div class="grid grid-cols-[15%_85%] grid-flow-row gap-2 items-center">
+      {#each card_points as point}
+        <img
+          src={PawBP}
+          alt="" />
+        <li class="list-none">{point}</li>
+      {/each}
+    </div>
+    <div class="p-4 {additional_info === '' ? 'hidden' : ''}">
+      <p>{additional_info}</p>
+    </div>
+  </div>
+</div>
